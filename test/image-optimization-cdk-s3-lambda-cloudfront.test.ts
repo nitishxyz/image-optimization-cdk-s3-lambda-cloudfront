34b4a7478c70dc91fbfcb966d42656eb4f1922cb
Nitish Sharma <nitishxyz@proton.me>
2024-01-27 23:52:38 +0530

    Initial commit

---
+++ test/image-optimization-cdk-s3-lambda-cloudfront.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ImageOptimizationCdkS3LambdaCloudfront from '../lib/image-optimization-cdk-s3-lambda-cloudfront-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/image-optimization-cdk-s3-lambda-cloudfront-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ImageOptimizationCdkS3LambdaCloudfront.ImageOptimizationCdkS3LambdaCloudfrontStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
